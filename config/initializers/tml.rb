0670529d96ad835e9ee16d4855c90e228ea3b4ef
Michael Berkovich <michael@translationexchange.com>
2015-12-07 14:46:51 -0800

    latest tml rails SDK

--- config/initializers/tml.rb
+++ config/initializers/tml.rb
@@ -1,6 +1,6 @@
 Tml.configure do |config|
 
-  if true or Rails.env.production?
+  if Rails.env.production?
     config.application = {
       key:    'b8206d1b982a5410f34da48490d7653fe25266f16c70482ccc953fb7264547d1',
       token:  '573d1cb5d207b439abdcf1cad8ff76c56b00c275fa032a69f748b34c82613e07'
@@ -12,11 +12,19 @@ Tml.configure do |config|
       host:       'tememcached.yptuob.cfg.usw1.cache.amazonaws.com:11211',
       namespace:  'b8206d1b982a5410f34da48490d7653fe25266f16c70482ccc953fb7264547d1',
     }
+
+    config.agent = {
+        enabled:  true,
+        type:     'agent',
+        host:     'https://staging-tools.translationexchange.com/agent/agent.min.js',
+        cache:    5.minutes
+    }
+
   else
     config.application = {
         host:   'http://localhost:3000',
-        key:    '096e039beebf4237f6829040276c7851996b2c8601abc6c4225d215d32b4c12c',
-        token:  '86cb4f636d23def23c348c119d4e8515bf188967d902ffeca5eed56e3a6c2f63'
+        key:    'b8206d1b982a5410f34da48490d7653fe25266f16c70482ccc953fb7264547d1',
+        token:  '573d1cb5d207b439abdcf1cad8ff76c56b00c275fa032a69f748b34c82613e07'
     }
 
     config.cache = {
@@ -24,6 +32,13 @@ Tml.configure do |config|
       adapter:    'file',
       path:       'config/tml',
       version:    'current'
+    }
+
+    config.agent = {
+        enabled:  true,
+        type:     'agent',
+        host:     'http://localhost:8282/dist/agent.js',
+        cache:    5.minutes
     }
   end
 
